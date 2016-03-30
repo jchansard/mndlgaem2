d6ba304e76ac20ef0b875b559edfdefd1f52ebc1
jchansard <josh.chansard@gmail.com>
2016-03-29 22:55:28 -0500

    added

---
+++ js/display/drawareas.js
@@ -0,0 +1,40 @@
+/**
+ * screenareas.js
+ *
+ * areas of canvas; akin to relatively-positioned divs within the parent canvas
+ *
+ * -------------
+ * Josh Chansard 
+ * https://github.com/jchansard/mndlgaem2
+ */
+
+module.exports = [{
+	name: 'full',
+	x: 0,
+	y: 0,
+	width: 60, 
+	height: 20, 
+	layer: 0
+}, {
+	name: 'mapterminal',
+	x: 1,
+	y: 1,
+	width: 60,
+	height: 20,
+	layer: 0
+}, {
+	name: 'skillterminal',
+	x: 0,
+	y: 20,
+	width: 40,
+	height: 6,
+	layer: 0
+},
+{
+	name: 'handterminal',
+	x: 0,
+	y: 26,
+	width: 40,
+	height: 10,
+	layer: 0
+}];
